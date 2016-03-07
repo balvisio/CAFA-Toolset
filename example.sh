01ba6639a10ef597f6c198754c4ec5e489a44910
arkatebi <arkatebi@gmail.com>
2016-03-07 12:55:30 -0600

    Added CAFA-Tools commands

--- example.sh
+++ example.sh
@@ -7,17 +7,17 @@
 # found in uniprot_sprot.dat.38 but not in gene_association.goa_ref_yeast.38+sprot.38 
 # at the end of the second file
 
-python Mergedb -I1=uniprot_sprot.dat.38 -I2=gene_association.goa_ref_yeast.38 -G 559292 
+python Mergedb -I1=uniprot_sprot.dat.2014_09 -I2=gene_association.goa_ref_yeast.38 -G 559292 
 
-# Output file: gene_association.goa_ref_yeast.38+sprot.38  
+# Output file: gene_association.goa_ref_yeast.38+sprot.2014_09  
 
 # Target Generation 
 # The following command will create a target file with the protein sequences from the input 
-# file uniprot_sprot.dat.38
+# file uniprot_sprot.dat.2014_09
 
-python Filter -I1=uniprot_sprot.dat.38  -G=559292 
+python Filter -I1=uniprot_sprot.dat.2014_09  -G=559292 
 
-# Output file name: uniprot_sprot.dat.38.559292.tfa.1 
+# Output file name: uniprot_sprot.dat.2014_09.559292.tfa.1 
 
 ## Benchmark Creation
 # The following command creates benchmark files based on two input annotation files
