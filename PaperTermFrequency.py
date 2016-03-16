51e8ca33c37d414797fbcc2f2237c25474474110
arkatebi <arkatebi@gmail.com>
2016-03-16 15:13:49 -0500

    Simplified code for both methods

--- PaperTermFrequency.py
+++ PaperTermFrequency.py
@@ -13,62 +13,44 @@ import re
    out high throughput papers (if required) and the second helps to identify 
    confident annotations. The assumption is annotations in more papers are
    considered confident over others.
+
+    count_freq: This method calculates two things: (1) the number of 
+    annotations per paper for every paper listed in the input file, and 
+    (2) how many papers are associated with every protein annotation pair.
+    It returns these two values as tuple of two dictionaries.
+
+    paper_term_freq: It populates the paper term frequency file. Then, it 
+    returns the  
 '''
 
-def count_freq(infile, EEC=set([]), ann_conf_filter=False, paper_conf_filter=False):
+def count_freq(goa_handle, EEC=set([])):
+#    print 'EEC: ' + str(EEC)
     paper_conf = defaultdict(lambda:defaultdict(set))
     ann_conf = defaultdict(lambda:defaultdict(set))
-    infile_handle = open(infile, 'r')
-
-    print 'Creating paper-term frequency file.....'
-
-    for line in infile_handle:
+    for line in goa_handle:
         if line[0] == '!':
             continue
         fields = line.strip().split('\t')
-        if not fields[5] == '' and re.match('^PMID', fields[5]):
-            paper_id = fields[5].split(':')[1]
-        
+        if not fields[5] == '' and re.match('^PMID', fields[5]): # Match PMID 
+            paper_id = fields[5].split(':')[1] # Extract PubMed id
             if (not EEC) or (fields[6] in EEC):
-                if ann_conf_filter and paper_conf_filter:
-                    ann_conf[fields[1]][fields[4]].add(str(paper_id))
-                    paper_conf[paper_id][fields[4]] = 1
-                elif ann_conf_filter:
-                    ann_conf[fields[1]][fields[4]].add(str(paper_id))
-                elif paper_conf_filter:
-                    paper_conf[paper_id][fields[4]] = 1
-    infile_handle.close()
-    return ann_conf, paper_conf
+                ann_conf[fields[1]][fields[4]].add(str(paper_id)) 
+                    # add pubmed id as evidence to the protein, GO ID 
+                    # (fields[1], fields[4]) pair
+                paper_conf[paper_id][fields[4]] = 1
+    return (ann_conf, paper_conf)
 
-def paper_term_freq(params, infile):
+def paper_term_freq(params, goa_handle, ptf_handle):
     # Given an input uniprot-goa file, this method computes the number of 
-    # proteins annotated in every pubmed id mentioned in that file
-    if params['Confidence'] == 'T':
-        paper_threshold = params['Threshold']
-        ann_conf_filter = True
-    else:
-        ann_conf_filter = False
-        paper_threshold = 0
+    # proteins annotated in every pubmed id mentioned in the file
 
-    if not os.path.exists(infile + '_with_annotations_per_paper.txt'):
-        paper_annotation_freq  = infile + '_with_annotations_per_paper.txt'
-        paper_conf_filter = True
-        paper_ann_freq_handle = open(paper_annotation_freq, 'w')
-    else:
-        paper_conf_filter = False
-    
-    if ann_conf_filter or paper_conf_filter:
-        [ann_conf, paper_conf] = count_freq(infile, params['Evidence'], 
-                                                     ann_conf_filter, paper_conf_filter)
-#        print ann_conf
-#        print paper_conf
-#        print 'here'
-        if len(paper_conf) > 0:
-            for i in paper_conf:
-                print >> paper_ann_freq_handle, i + '\t' + str(len(paper_conf[i]))
-            paper_conf.clear()
-    else:
-        ann_conf = defaultdict(lambda:defaultdict(set))
+    ann_conf, paper_conf = count_freq(goa_handle,
+                            params['Evidence'])
+    print 'Populating paper-term frequency file ...'
+    if len(paper_conf) > 0:
+        for pubmed_id in paper_conf:
+            print >> ptf_handle, pubmed_id + '\t' + str(len(paper_conf[pubmed_id]))
+    paper_conf.clear()
     return ann_conf
 
 if __name__ == '__main__':
