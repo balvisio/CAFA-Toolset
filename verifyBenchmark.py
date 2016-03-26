e964e25cc4ab494ce468497bf0c8d07a14a06e2a
arkatebi <arkatebi@gmail.com>
2016-03-26 10:19:54 -0500

    Generalized methods to make them reuseable

--- verifyBenchmark.py
+++ verifyBenchmark.py
@@ -5,14 +5,46 @@ import os.path
 
 '''
     This script verifies the Limited-Knowledge(LK) and No-Knowledge(NK) 
-    benchmark sets created by CreateBenchmark program.
+    benchmark sets created by CreateBenchmark program. The entry points 
+    for such verification are the following two methods: 
+        verify_LK_benchmark: This method need to be invoked to verify 
+    LK-benchmark files. The arguments for calling this method are the 
+    following: 
+               t1_iea_handle: file handle for t1_ea file
+               t1_exp_handle: file handle for t1_exp file 
+               t2_exp_handle: file handle for t2_exp file
+               output_filename_LK_bpo: file name for LK-bpo benchmark set
+               output_filename_LK_cco: file name for LK-cco benchmark set
+               output_filename_LK_mfo: file name for LK-mfo benchmark set
+
+        verify_NK_benchmark: This method need to be invoked to verify 
+    NK-benchmark files. The arguments for calling this method are the 
+    following: 
+               t1_iea_handle: file handle for t1_ea file
+               t1_exp_handle: file handle for t1_exp file 
+               t2_exp_handle: file handle for t2_exp file
+               output_filename_LK_bpo: file name for NK-bpo benchmark set
+               output_filename_LK_cco: file name for NK-cco benchmark set
+               output_filename_LK_mfo: file name for NK-mfo benchmark set
+
+       The following methods are invoked by the above two methods to create 
+    data structure and perform the verification:
+       create_iea_dict: This method builds a dictionary for <protein, GO ID> 
+                        tuples from t1_iea file.
+       create_exp_dict: This method builds three dictionaries in BPO, CCO, and 
+                        MFO categories for <protein, GO ID> tuples from a t1_exp 
+                        or t2_exp file.  
+       verify_LK_benchmark_xxo: This method verifies any of the LK-benchmark 
+                        sets.
+       verify_NK_benchmark_xxo: This method verifies any of the NK-benchmark 
+                        sets
 '''
 
 def create_iea_dict(iea_handle):
      # Initialize a dictionar which will later be populated with
     # <protein, GO ID> tuples from t1_iea file:
     dict_iea = defaultdict(lambda:set())
-    # Populate the dictionar for t1_iea with <protein, GO terms> as
+    # Populate the dictionary for t1_iea with <protein, GO terms> as
     # <key, values> pairs from the entries with NOn-Experimental Evidence
     # at time t1:
     for lines in iea_handle:
@@ -47,210 +79,69 @@ def create_exp_dict(exp_handle):
             dict_cco[cols[1]].add(cols[4])
     return (dict_bpo, dict_cco, dict_mfo)
 
-def verify_LK_benchmark_bpo(t1_iea_dict,
-                            t1_bpo_dict,
-                            t1_cco_dict,
-                            t1_mfo_dict,
-                            t2_bpo_dict,
-                            t2_cco_dict,
-                            t2_mfo_dict,
-                            output_filename_LK_bpo,
+
+def verify_LK_benchmark_xxo(t1_iea_dict,
+                            t1_xxo_dict,
+                            t2_xxo_dict,
+                            output_filename_LK_xxo, # benchmark file name
                             LKcount):
-    # Verify the benchmark entries in the benchmark file output_filename_LK_bpo:
-    if os.path.exists(output_filename_LK_bpo) and \
-       os.stat(output_filename_LK_bpo).st_size != 0:
-        outfile_LK_bpo_handle = open(output_filename_LK_bpo, 'r')
-        for lines in outfile_LK_bpo_handle: 
+    # This method verifies the benchmark entries in the benchmark file 
+    # output_filename_LK_xxo.
+    # Meaning of xxo: When invoked, xxo need to be replaced by bpo, cco, or mfo to 
+    #                 make this method specific to a certain type of benchmarks 
+    if os.path.exists(output_filename_LK_xxo) and \
+       os.stat(output_filename_LK_xxo).st_size != 0:
+        outfile_LK_xxo_handle = open(output_filename_LK_xxo, 'r')
+        for lines in outfile_LK_xxo_handle:
             cols = lines.strip().split('\t')
             if cols[0] not in t1_iea_dict:
                 print 'Error: an undesired protein ' + cols[0] + \
-                      ' got selected in ' + output_filename_LK_bpo
+                      ' got selected in ' + output_filename_LK_xxo
                 break
-            elif cols[0] in t1_bpo_dict:
+            elif cols[0] in t1_xxo_dict:
                 print 'Error: selected protein ' + cols[0] + ' in ' + \
-                       output_filename_LK_bpo + ' already had experimental \
+                       output_filename_LK_xxo + ' already had experimental \
                        evidence at t1'
                 break
-            elif cols[0] not in t2_bpo_dict or cols[1] not in t2_bpo_dict[cols[0]]:
+            elif cols[0] not in t2_xxo_dict or cols[1] not in t2_xxo_dict[cols[0]]:
                 print 'Error: selected protein ' + cols[0] + ' in ' + \
-                      output_filename_LK_bpo + ' has not gained experimental \
+                      output_filename_LK_xxo + ' has not gained experimental \
                       evidence at t2'
                 break 
-        outfile_LK_bpo_handle.close() 
+        outfile_LK_xxo_handle.close() 
         LKcount += 1
     return LKcount    
 
-def verify_LK_benchmark_cco(t1_iea_dict,
+def verify_NK_benchmark_xxo(t1_iea_dict,
                             t1_bpo_dict,
                             t1_cco_dict,
                             t1_mfo_dict,
-                            t2_bpo_dict,
-                            t2_cco_dict,
-                            t2_mfo_dict,
-                            output_filename_LK_cco,
-                            LKcount):
-    # Verify the benchmark entries in the benchmark file output_filename_LK_cco:
-    if os.path.exists(output_filename_LK_cco) and \
-       os.stat(output_filename_LK_cco).st_size != 0:
-
-        outfile_LK_cco_handle = open(output_filename_LK_cco, 'r')
-        for lines in outfile_LK_cco_handle:
-            cols = lines.strip().split('\t')
-            if cols[0] not in t1_iea_dict:
-                print 'Error: an undesired protein ' + cols[0] + \
-                      ' got selected in ' + output_filename_LK_cco
-                break
-            elif cols[0] in t1_cco_dict:
-                print 'Error: selected protein ' + cols[0] + ' in ' + \
-                      output_filename_LK_cco + ' already had experimental \
-                      evidence at t1'
-                break
-            elif cols[0] not in t2_cco_dict or \
-                 cols[1] not in t2_cco_dict[cols[0]]:
-                print 'Error: selected protein ' + cols[0] + ' in ' + \
-                      output_filename_LK_cco + ' has not gained experimental \
-                      evidence at t2'
-                break
-        outfile_LK_cco_handle.close()
-        LKcount += 1
-    return LKcount
-
-def verify_LK_benchmark_mfo(t1_iea_dict,
-                            t1_bpo_dict,
-                            t1_cco_dict,
-                            t1_mfo_dict,
-                            t2_bpo_dict,
-                            t2_cco_dict,
-                            t2_mfo_dict,
-                            output_filename_LK_mfo,
-                            LKcount):
-    # Verify the benchmark entries in the benchmark file output_filename_LK_mfo:
-    if os.path.exists(output_filename_LK_mfo) and \
-       os.stat(output_filename_LK_mfo).st_size != 0:
-        outfile_LK_mfo_handle = open(output_filename_LK_mfo, 'r')
-        for lines in outfile_LK_mfo_handle:
-            cols = lines.strip().split('\t')
-            if cols[0] not in t1_iea_dict:
-                  print 'Error: an undesired protein ' + cols[0] + \
-                        ' got selected in ' + output_filename_LK_mfo
-                  break
-            elif cols[0] in t1_mfo_dict:
-                  print 'Error: selected protein ' + cols[0] + ' in ' + \
-                        output_filename_LK_mfo + ' already had experimental \
-                        evidence at t1'
-                  break
-            elif cols[0] not in t2_mfo_dict or \
-                 cols[1] not in t2_mfo_dict[cols[0]]:
-                  print 'Error: selected protein ' + cols[0] + ' in ' + \
-                        output_filename_LK_mfo + ' has not gained experimental \
-                        evidence at t2'
-                  break
-        outfile_LK_mfo_handle.close()
-        LKcount += 1
-    return LKcount
-
-def verify_NK_benchmark_bpo(t1_iea_dict,
-                            t1_bpo_dict,
-                            t1_cco_dict,
-                            t1_mfo_dict,
-                            t2_bpo_dict,
-                            t2_cco_dict,
-                            t2_mfo_dict,
-                            output_filename_NK_bpo,
+                            t2_xxo_dict,
+                            output_filename_NK_xxo,
                             NKcount):
-    # Verify the benchmark entries in the benchmark file: output_filename_NK_bpo
-    if os.path.exists(output_filename_NK_bpo) and \
-       os.stat(output_filename_NK_bpo).st_size != 0:
-        outfile_NK_bpo_handle = open(output_filename_NK_bpo, 'r')
-        for lines in outfile_NK_bpo_handle: 
+    # Verify the benchmark entries in the benchmark file: output_filename_NK_xxo
+    if os.path.exists(output_filename_NK_xxo) and \
+       os.stat(output_filename_NK_xxo).st_size != 0:
+        outfile_NK_xxo_handle = open(output_filename_NK_xxo, 'r')
+        for lines in outfile_NK_xxo_handle: 
             cols = lines.strip().split('\t')
             if cols[0] not in t1_iea_dict:
                 print 'Error: an undesired protein ' + cols[0] + \
-                      ' got selected in ' + output_filename_NK_bpo
+                      ' got selected in ' + output_filename_NK_xxo
                 break
             elif cols[0] in t1_bpo_dict or \
                  cols[0] in t1_cco_dict or \
                  cols[0] in t1_mfo_dict:
                 print 'Error: selected protein ' + cols[0] + ' in ' + \
-                      output_filename_NK_bpo + ' already had experimental \
+                      output_filename_NK_xxo + ' already had experimental \
                       evidence at t1'
                 break
-            elif cols[0] not in t2_bpo_dict:
+            elif cols[0] not in t2_xxo_dict:
                 print 'Error: selected protein ' + cols[0] + ' in ' + \
-                      output_filename_NK_bpo + ' has not gained experimental \
+                      output_filename_NK_xxo + ' has not gained experimental \
                       evidence at t2'
                 break 
-        outfile_NK_bpo_handle.close() 
-        NKcount += 1
-    return NKcount
-
-def verify_NK_benchmark_cco(t1_iea_dict,
-                            t1_bpo_dict,
-                            t1_cco_dict,
-                            t1_mfo_dict,
-                            t2_bpo_dict,
-                            t2_cco_dict,
-                            t2_mfo_dict,
-                            output_filename_NK_cco,
-                            NKcount):
-    # Verify the benchmark entries in the benchmark file: output_filename_NK_cco
-    if os.path.exists(output_filename_NK_cco) and \
-       os.stat(output_filename_NK_cco).st_size != 0:
-        outfile_NK_cco_handle = open(output_filename_NK_cco, 'r')
-        for lines in outfile_NK_cco_handle:
-            cols = lines.strip().split('\t')
-            if cols[0] not in t1_iea_dict:
-                print 'Error: an undesired protein ' + cols[0] + \
-                      ' got selected in ' + output_filename_NK_cco
-                break
-            elif cols[0] in t1_bpo_dict or \
-                 cols[0] in t1_cco_dict or \
-                 cols[0] in t1_mfo_dict:
-                print 'Error: selected protein ' + cols[0] + ' in ' + \
-                      output_filename_NK_cco + ' already had experimental \
-                      evidence at t1'
-                break
-            elif cols[0] not in t2_cco_dict:
-                print 'Error: selected protein ' + cols[0] + ' in ' + \
-                      output_filename_NK_cco + ' has not gained experimental \
-                      evidence at t2'
-                break 
-        outfile_NK_cco_handle.close() 
-        NKcount += 1
-    return NKcount
-
-def verify_NK_benchmark_mfo(t1_iea_dict,
-                            t1_bpo_dict,
-                            t1_cco_dict,
-                            t1_mfo_dict,
-                            t2_bpo_dict,
-                            t2_cco_dict,
-                            t2_mfo_dict,
-                            output_filename_NK_mfo,
-                            NKcount):
-    # Verify the benchmark entries in the benchmark file: output_filename_NK_mfo
-    if os.path.exists(output_filename_NK_mfo) and \
-       os.stat(output_filename_NK_mfo).st_size != 0:
-        outfile_NK_mfo_handle = open(output_filename_NK_mfo, 'r')
-        for lines in outfile_NK_mfo_handle:
-            cols = lines.strip().split('\t')
-            if cols[0] not in t1_iea_dict:
-                  print 'Error: an undesired protein ' + cols[0] + \
-                        ' got selected in ' + output_filename_NK_mfo
-                  break
-            elif cols[0] in t1_bpo_dict or \
-                 cols[0] in t1_cco_dict or \
-                 cols[0] in t1_mfo_dict:
-                  print 'Error: selected protein ' + cols[0] + ' in ' + \
-                        output_filename_NK_mfo + ' already had experimental \
-                        evidence at t1'
-                  break
-            elif cols[0] not in t2_mfo_dict:
-                  print 'Error: selected protein ' + cols[0] + ' in ' + \
-                        output_filename_NK_mfo + ' has not gained experimental \
-                        evidence at t2'
-                  break 
-        outfile_NK_mfo_handle.close() 
+        outfile_NK_xxo_handle.close() 
         NKcount += 1
     return NKcount
 
@@ -272,40 +163,28 @@ def verify_LK_benchmark(t1_iea_handle,
     t2_bpo_dict, t2_cco_dict, t2_mfo_dict = create_exp_dict(t2_exp_handle) 
    
     LKcount = 0
-    LKcount = LKcount + verify_LK_benchmark_bpo(t1_iea_dict,
+    LKcount = LKcount + verify_LK_benchmark_xxo(t1_iea_dict,
                                                 t1_bpo_dict,
-                                                t1_cco_dict,
-                                                t1_mfo_dict,
                                                 t2_bpo_dict,
-                                                t2_cco_dict,
-                                                t2_mfo_dict,
                                                 output_filename_LK_bpo,
                                                 LKcount)
-    LKcount = LKcount + verify_LK_benchmark_cco(t1_iea_dict,
-                                                t1_bpo_dict,
+    LKcount = LKcount + verify_LK_benchmark_xxo(t1_iea_dict,
                                                 t1_cco_dict,
-                                                t1_mfo_dict,
-                                                t2_bpo_dict,
                                                 t2_cco_dict,
-                                                t2_mfo_dict,
                                                 output_filename_LK_cco,
                                                 LKcount)
-    LKcount = LKcount + verify_LK_benchmark_mfo(t1_iea_dict,
-                                                t1_bpo_dict,
-                                                t1_cco_dict,
+    LKcount = LKcount + verify_LK_benchmark_xxo(t1_iea_dict,
                                                 t1_mfo_dict,
-                                                t2_bpo_dict,
-                                                t2_cco_dict,
                                                 t2_mfo_dict,
                                                 output_filename_LK_mfo,
                                                 LKcount)
     return LKcount
 
-def verify_NK_benchmark(t1_iea_handle, 
-                        t1_exp_handle, 
-                        t2_exp_handle, 
-                        output_filename_NK_bpo, 
-                        output_filename_NK_cco, 
+def verify_NK_benchmark(t1_iea_handle,
+                        t1_exp_handle,
+                        t2_exp_handle,
+                        output_filename_NK_bpo,
+                        output_filename_NK_cco,
                         output_filename_NK_mfo):
 
     # Create a dictionary for the <protein, GO ID> tuples from t1_iea file:
@@ -315,35 +194,33 @@ def verify_NK_benchmark(t1_iea_handle,
     # <protein, GO ID> tuples from t1_exp file:
     t1_bpo_dict, t1_cco_dict, t1_mfo_dict = create_exp_dict(t1_exp_handle)
 
-    # Create BPO, CCO and MFO dictionaries for the 
+    # Create BPO, CCO and MFO dictionaries for the
     # <protein, GO ID> tuples from t2_exp file:
     t2_bpo_dict, t2_cco_dict, t2_mfo_dict = create_exp_dict(t2_exp_handle)
 
     NKcount = 0
-    NKcount = NKcount + verify_NK_benchmark_bpo(t1_iea_dict,
+    # Verify NK-BPO benchmarks:
+    NKcount = NKcount + verify_NK_benchmark_xxo(t1_iea_dict,
                                                 t1_bpo_dict,
                                                 t1_cco_dict,
                                                 t1_mfo_dict,
                                                 t2_bpo_dict,
-                                                t2_cco_dict,
-                                                t2_mfo_dict,
                                                 output_filename_NK_bpo,
                                                 NKcount)
-    NKcount = NKcount + verify_NK_benchmark_cco(t1_iea_dict,
+    # Verify NK-CCO benchmarks:
+    NKcount = NKcount + verify_NK_benchmark_xxo(t1_iea_dict,
                                                 t1_bpo_dict,
                                                 t1_cco_dict,
                                                 t1_mfo_dict,
-                                                t2_bpo_dict,
                                                 t2_cco_dict,
-                                                t2_mfo_dict,
                                                 output_filename_NK_cco,
                                                 NKcount)
-    NKcount = NKcount + verify_NK_benchmark_mfo(t1_iea_dict,
+
+    # Verify NK-MFO benchmarks:
+    NKcount = NKcount + verify_NK_benchmark_xxo(t1_iea_dict,
                                                 t1_bpo_dict,
                                                 t1_cco_dict,
                                                 t1_mfo_dict,
-                                                t2_bpo_dict,
-                                                t2_cco_dict,
                                                 t2_mfo_dict,
                                                 output_filename_NK_mfo,
                                                 NKcount)
