958c5f641454d30c80733f0f2c61537ab9504aac
arkatebi <arkatebi@gmail.com>
2016-03-27 14:35:39 -0500

    Added docstring

--- verifyBenchmark.py
+++ verifyBenchmark.py
@@ -1,79 +1,90 @@
 #!/usr/bin/env python
 
+"""
+    This script verifies the Limited-Knowledge(LK) and No-Knowledge(NK)
+    benchmark sets. The entry points for such verification are the
+    following two methods:
+
+        verify_LK_benchmark: 
+            This method need to be invoked to verify LK-benchmark files.
+            The arguments for calling this method are the following:
+                t1_iea_handle: file handle to a t1_ea file
+                t1_exp_handle: file handle to a t1_exp file
+                t2_exp_handle: file handle to a t2_exp file
+                output_filename_LK_bpo: file name to a LK-bpo benchmark set
+                output_filename_LK_cco: file name to a LK-cco benchmark set
+                output_filename_LK_mfo: file name to a LK-mfo benchmark set
+
+        verify_NK_benchmark: 
+            This method need to be invoked to verify NK-benchmark files. The 
+            arguments for calling this method are the following:
+                t1_iea_handle: file handle to a t1_ea file
+                t1_exp_handle: file handle to a t1_exp file
+                t2_exp_handle: file handle to a t2_exp file
+                output_filename_LK_bpo: file name to a NK-bpo benchmark set
+                output_filename_LK_cco: file name to a NK-cco benchmark set
+                output_filename_LK_mfo: file name to a NK-mfo benchmark set
+
+        The following methods are invoked by the above two methods to create
+        required data structures and perform the verification:
+
+       create_iea_ann_dict: 
+                This method builds a dictionary for <protein, GO ID> tuples 
+                from t1_iea file.
+
+       create_ann_ann_dict: 
+                This method builds three dictionaries in BPO, CCO, and MFO 
+                categories for <protein, GO ID> tuples from a 
+                t1_exp or t2_exp file.
+
+       verify_LK_benchmark_xxo: 
+               This method verifies any of the LK-benchmark sets.
+
+       verify_NK_benchmark_xxo: 
+               This method verifies any of the NK-benchmark sets.
+"""
+
 import os.path
 import sys
 from collections import defaultdict
 
-'''
-    This script verifies the Limited-Knowledge(LK) and No-Knowledge(NK) 
-    benchmark sets created by CreateBenchmark program. The entry points 
-    for such verification are the following two methods: 
-
-        verify_LK_benchmark: This method need to be invoked to verify 
-    LK-benchmark files. The arguments for calling this method are the 
-    following: 
-               t1_iea_handle: file handle for t1_ea file
-               t1_exp_handle: file handle for t1_exp file 
-               t2_exp_handle: file handle for t2_exp file
-               output_filename_LK_bpo: file name for LK-bpo benchmark set
-               output_filename_LK_cco: file name for LK-cco benchmark set
-               output_filename_LK_mfo: file name for LK-mfo benchmark set
-
-        verify_NK_benchmark: This method need to be invoked to verify 
-    NK-benchmark files. The arguments for calling this method are the 
-    following: 
-               t1_iea_handle: file handle for t1_ea file
-               t1_exp_handle: file handle for t1_exp file 
-               t2_exp_handle: file handle for t2_exp file
-               output_filename_LK_bpo: file name for NK-bpo benchmark set
-               output_filename_LK_cco: file name for NK-cco benchmark set
-               output_filename_LK_mfo: file name for NK-mfo benchmark set
-
-       The following methods are invoked by the above two methods to create 
-    data structure and perform the verification:
-       create_iea_dict: This method builds a dictionary for <protein, GO ID> 
-                        tuples from t1_iea file.
-       create_exp_dict: This method builds three dictionaries in BPO, CCO, and 
-                        MFO categories for <protein, GO ID> tuples from a t1_exp 
-                        or t2_exp file.
-       verify_LK_benchmark_xxo: This method verifies any of the LK-benchmark 
-                        sets.
-       verify_NK_benchmark_xxo: This method verifies any of the NK-benchmark 
-                        sets.
-'''
-
-def create_iea_dict(iea_handle):
+def create_iea_ann_dict(goa_iea_handle):
      # Initialize a dictionar which will later be populated with
     # <protein, GO ID> tuples from t1_iea file:
     dict_iea = defaultdict(lambda:set())
     # Populate the dictionary for t1_iea with <protein, GO terms> as
     # <key, values> pairs from the entries with NOn-Experimental Evidence
     # at time t1:
-    for lines in iea_handle:
+    for lines in goa_iea_handle:
         cols = lines.strip().split('\t')
         if len(cols) < 15:
             continue
         dict_iea[cols[1]].add(cols[4])
-        # Column 1: protein name, Column 4: GO ID 
+        # Column 1: protein name, Column 4: GO ID
     return dict_iea
 
-def create_exp_dict(exp_handle):
+def create_exp_ann_dict(goa_exp_handle):
+    """
+    This method builds three dictionaries in BPO, CCO, and MFO categories
+    for <protein, GO ID> tuples from UniProt-GOA file without the header
+    string.
+    """
     # Initialize three dictionaries in BPO, CCO, and MFO ontology groups
     # which will later be populated with <protein, GO ID> tuples from
-    # t1_exp file ontology groups: 
+    # t1_exp file ontology groups:
     dict_bpo = defaultdict(lambda:set())
     dict_cco = defaultdict(lambda:set())
-    dict_mfo = defaultdict(lambda:set()) 
+    dict_mfo = defaultdict(lambda:set())
 
-    # Populate the three dictionaries for t1_exp with <protein, GO terms> 
-    # as <key, values> pairs from the entries with Non-Experimental Evidence 
+    # Populate the three dictionaries for t1_exp with <protein, GO terms>
+    # as <key, values> pairs from the entries with Non-Experimental Evidence
     # at time t1:
-    for lines in exp_handle:
+    for lines in goa_exp_handle:
         cols = lines.strip().split('\t')
         if len(cols) < 15:
             continue
         if cols[8] == 'F': # Column 8: Ontology group
-            dict_mfo[cols[1]].add(cols[4]) 
+            dict_mfo[cols[1]].add(cols[4])
             # Column 1: protein name, Column 4: GO ID 
         elif cols[8] == 'P':
             dict_bpo[cols[1]].add(cols[4])
@@ -81,16 +92,18 @@ def create_exp_dict(exp_handle):
             dict_cco[cols[1]].add(cols[4])
     return (dict_bpo, dict_cco, dict_mfo)
 
-
 def verify_LK_benchmark_xxo(t1_iea_dict,
                             t1_xxo_dict,
                             t2_xxo_dict,
                             output_filename_LK_xxo, # benchmark file name
                             LKcount):
-    # This method verifies the benchmark entries in the benchmark file 
-    # output_filename_LK_xxo.
-    # Meaning of xxo: When invoked, xxo need to be replaced by bpo, cco, or mfo to 
-    #                 make this method specific to a certain type of benchmarks 
+    """
+    This method verifies the benchmark entries in the benchmark file
+    output_filename_LK_xxo.
+
+    Meaning of xxo: xxo is replaced runtime by bpo, cco, or mfo to make 
+    this method specific to a certain type of benchmarks.
+    """
     if os.path.exists(output_filename_LK_xxo) and \
        os.stat(output_filename_LK_xxo).st_size != 0:
         outfile_LK_xxo_handle = open(output_filename_LK_xxo, 'r')
@@ -109,10 +122,10 @@ def verify_LK_benchmark_xxo(t1_iea_dict,
                 print 'Error: selected protein ' + cols[0] + ' in ' + \
                       output_filename_LK_xxo + ' has not gained experimental \
                       evidence at t2'
-                break 
-        outfile_LK_xxo_handle.close() 
+                break
+        outfile_LK_xxo_handle.close()
         LKcount += 1
-    return LKcount    
+    return LKcount
 
 def verify_NK_benchmark_xxo(t1_iea_dict,
                             t1_bpo_dict,
@@ -121,11 +134,18 @@ def verify_NK_benchmark_xxo(t1_iea_dict,
                             t2_xxo_dict,
                             output_filename_NK_xxo,
                             NKcount):
-    # Verify the benchmark entries in the benchmark file: output_filename_NK_xxo
+    """ 
+    This method verify the benchmark entries in the benchmark file 
+    output_filename_NK_xxo.
+
+    Meaning of xxo: xxo is replaced runtime by bpo, cco, or mfo to make 
+    this method specific to a certain type of benchmarks.
+
+    """
     if os.path.exists(output_filename_NK_xxo) and \
        os.stat(output_filename_NK_xxo).st_size != 0:
         outfile_NK_xxo_handle = open(output_filename_NK_xxo, 'r')
-        for lines in outfile_NK_xxo_handle: 
+        for lines in outfile_NK_xxo_handle:
             cols = lines.strip().split('\t')
             if cols[0] not in t1_iea_dict:
                 print 'Error: an undesired protein ' + cols[0] + \
@@ -142,8 +162,8 @@ def verify_NK_benchmark_xxo(t1_iea_dict,
                 print 'Error: selected protein ' + cols[0] + ' in ' + \
                       output_filename_NK_xxo + ' has not gained experimental \
                       evidence at t2'
-                break 
-        outfile_NK_xxo_handle.close() 
+                break
+        outfile_NK_xxo_handle.close()
         NKcount += 1
     return NKcount
 
@@ -153,16 +173,19 @@ def verify_LK_benchmark(t1_iea_handle,
                         output_filename_LK_bpo,
                         output_filename_LK_cco,
                         output_filename_LK_mfo):
+    """ 
+    This method verifies Limited-Knowledge benchmark sets.
+    """
     # Create a dictionary for the <protein, GO ID> tuples from t1_iea file:
-    t1_iea_dict = create_iea_dict(t1_iea_handle)
+    t1_iea_dict = create_iea_ann_dict(t1_iea_handle)
 
-    # Create BPO, CCO and MFO dictionaries for the 
+    # Create BPO, CCO and MFO dictionaries for the
     # <protein, GO ID> tuples from t1_exp file:
-    t1_bpo_dict, t1_cco_dict, t1_mfo_dict = create_exp_dict(t1_exp_handle) 
+    t1_bpo_dict, t1_cco_dict, t1_mfo_dict = create_exp_ann_dict(t1_exp_handle)
 
-    # Create BPO, CCO and MFO dictionaries for the 
+    # Create BPO, CCO and MFO dictionaries for the
     # <protein, GO ID> tuples from t2_exp file:
-    t2_bpo_dict, t2_cco_dict, t2_mfo_dict = create_exp_dict(t2_exp_handle) 
+    t2_bpo_dict, t2_cco_dict, t2_mfo_dict = create_exp_ann_dict(t2_exp_handle)
    
     LKcount = 0
     LKcount = LKcount + verify_LK_benchmark_xxo(t1_iea_dict,
@@ -188,17 +211,19 @@ def verify_NK_benchmark(t1_iea_handle,
                         output_filename_NK_bpo,
                         output_filename_NK_cco,
                         output_filename_NK_mfo):
-
+    """
+    This method verifies No-Knowledge benchmark sets.
+    """
     # Create a dictionary for the <protein, GO ID> tuples from t1_iea file:
-    t1_iea_dict = create_iea_dict(t1_iea_handle)
+    t1_iea_dict = create_iea_ann_dict(t1_iea_handle)
 
-    # Create BPO, CCO and MFO dictionaries for the 
+    # Create BPO, CCO and MFO dictionaries for the
     # <protein, GO ID> tuples from t1_exp file:
-    t1_bpo_dict, t1_cco_dict, t1_mfo_dict = create_exp_dict(t1_exp_handle)
+    t1_bpo_dict, t1_cco_dict, t1_mfo_dict = create_exp_ann_dict(t1_exp_handle)
 
     # Create BPO, CCO and MFO dictionaries for the
     # <protein, GO ID> tuples from t2_exp file:
-    t2_bpo_dict, t2_cco_dict, t2_mfo_dict = create_exp_dict(t2_exp_handle)
+    t2_bpo_dict, t2_cco_dict, t2_mfo_dict = create_exp_ann_dict(t2_exp_handle)
 
     NKcount = 0
     # Verify NK-BPO benchmarks:
@@ -228,6 +253,7 @@ def verify_NK_benchmark(t1_iea_handle,
     return NKcount
 
 if __name__ == "__main__":
-    print 'This script does not run independently'
+    print (sys.argv[0] + ' docstring:')
+    print (__doc__)
     sys.exit(1)
 
