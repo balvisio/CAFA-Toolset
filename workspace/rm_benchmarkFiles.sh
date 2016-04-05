f60f815af8a0b265c60e5bb40b427df80fdf3f0d
arkatebi <arkatebi@gmail.com>
2016-04-05 08:13:12 -0500

    Added script that deletes benchmark files from testBenchmark

---
+++ workspace/rm_benchmarkFiles.sh
@@ -0,0 +1,15 @@
+rm gene_association.goa_ref_arabidopsis.127-95*
+rm gene_association.goa_ref_chicken.124-92*
+rm gene_association.goa_ref_cow.117-85*
+rm gene_association.goa_ref_dicty.55-23*
+rm gene_association.goa_ref_dog.55-23*
+rm gene_association.goa_ref_fly.55-23*
+rm gene_association.goa_ref_human.154-122*
+rm gene_association.goa_ref_mouse.140-108*
+rm gene_association.goa_ref_pig.55-23*
+rm gene_association.goa_ref_rat.140-108*
+rm gene_association.goa_ref_worm.55-23*
+rm gene_association.goa_ref_yeast.55-23*
+rm gene_association.goa_ref_zebrafish.127-95*
+rm gene_association.goa_ref_uniprot.152-121*
+
