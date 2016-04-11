2b2be3b79d1106f4bb569e4831e966513f8e7dd1
arkatebi <arkatebi@gmail.com>
2016-04-11 18:56:05 -0500

    Added methods for DB-REF

--- AppendSprot2GOA.py
+++ AppendSprot2GOA.py
@@ -50,24 +50,28 @@ from Bio import SwissProt as sp
 import GOAParser
 import GOAParser_cafa as gc
 
-Months = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+Months = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', \
+              'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 def assignSymbol(sprotRec): 
     symbol = '' 
 # scenario 1: gene_name=> Name=DBP5; Synonyms=RAT8; OrderedLocusNames=YOR046C;
-# scenario 2: gene_name=> Name=DCS2 {ECO:0000312|SGD:S000005699}; OrderedLocusNames=YOR173W; ORFNames=O3625;
+# scenario 2: gene_name=> Name=DCS2 {ECO:0000312|SGD:S000005699}; 
+#                         OrderedLocusNames=YOR173W; ORFNames=O3625;
 # scenario 3: gene_name=> Name=DDI2; OrderedLocusNames=YFL061W;
     for g in sprotRec.gene_name.strip(';').split(';'):
         if not g or g.find('=') == -1:
             continue 
-        for f in g.split('=')[1].split(','): # split each memmber of the group g to a list 
+        # Split each memmber of the group g to a list:
+        for f in g.split('=')[1].split(','):  
             symbol = ((f.split(' '))[0]).strip()
             break
-        if not symbol and not symbol.isspace():# the symbol happens to be not found in the above group g
+        # the symbol happens to be not found in the above group g:
+        if not symbol and not symbol.isspace():
             continue
         else:
             break
-    return symbol # symbol is extracted from gene_name field of sprotRec and then return
+    return symbol 
 
 def assignGO_REF(sprotRec, crossRef):
     go_ref = ''
@@ -105,17 +109,19 @@ def assignDB_REF(sprotRec, crossRef):
      
 def assignSynonym(sprotRec):
     synonym = []
-# scenario 1: entry_name=> DBP5_YEAST
-# scenario 2: entry_name=> DCS2_YEAST
-# scenario 3: entry_name=> DDI2_YEAST
+# Scenario 1: entry_name=> DBP5_YEAST
+# Scenario 2: entry_name=> DCS2_YEAST
+# Scenario 3: entry_name=> DDI2_YEAST
     synonym.append(sprotRec.entry_name)
-# scenario 1: gene_name=> Name=DBP5; Synonyms=RAT8; OrderedLocusNames=YOR046C;
-# scenario 2: gene_name=> Name=DCS2 {ECO:0000312|SGD:S000005699}; OrderedLocusNames=YOR173W; ORFNames=O3625;
-# scenario 3: gene_name=> Name=DDI2; OrderedLocusNames=YFL061W;
+# Scenario 1: gene_name=> Name=DBP5; Synonyms=RAT8; OrderedLocusNames=YOR046C;
+# Scenario 2: gene_name=> Name=DCS2 {ECO:0000312|SGD:S000005699}; 
+#                         OrderedLocusNames=YOR173W; ORFNames=O3625;
+# Scenario 3: gene_name=> Name=DDI2; OrderedLocusNames=YFL061W;
     for g in sprotRec.gene_name.strip(';').split(';'):
         if not g or g.find('=') == -1:
             continue 
-        for f in g.split('=')[1].split(','): # split each memmber of the group g to a list
+        # Split each memmber of the group g to a list:
+        for f in g.split('=')[1].split(','): 
             tmpStr = ((f.split(' '))[0]).strip()
             if not tmpStr and not tmpStr.isspace():
                 pass
@@ -138,7 +144,8 @@ def assignDate(sprotRec):
     mm_2 = ((((sprotRec.annotation_update[0]).split('-'))[1]).title())
     dd_2 =((sprotRec.annotation_update[0]).split('-'))[0]
   
-    if (datetime(int(yy_1), Months.index(mm_1),int(dd_1)) < datetime(int(yy_2), Months.index(mm_2), int(dd_2))):
+    if (datetime(int(yy_1), Months.index(mm_1),int(dd_1)) < \
+        datetime(int(yy_2), Months.index(mm_2), int(dd_2))):
         date = yy_2 + str(Months.index(mm_2)) + dd_2
     else:
         date = yy_1 + str(Months.index(mm_1)) + dd_1
@@ -146,12 +153,12 @@ def assignDate(sprotRec):
 
 def swissProt2GOA(sprotRec, crossRef, fields=GOAParser.GAF20FIELDS):
     """
-     This method takes a SwissProt record and GO term information 
-     as input arguments. It then constructs a GOA dictionary using 
-     the 'fields' as keys and values taken from sprotReci, and then 
+     This method takes a SwissProt record and GO term information
+     as input arguments. It then constructs a GOA dictionary using
+     the 'fields' as keys and values taken from sprotReci, and then
      returns the constructed GOA record.
     """
-    # 15 fields are defined for GAF10FIELDS (GAF 1.0)
+    # 15 fields are defined for GAF10FIELDS (GAF 1.0):
     goaRec = {'DB':'SwissProt', # 'SwissProt' is assigned to DB
               'DB_Object_ID': sprotRec.accessions[0],
               'DB_Object_Symbol': assignSymbol(sprotRec),
@@ -169,8 +176,8 @@ def swissProt2GOA(sprotRec, crossRef, fields=GOAParser.GAF20FIELDS):
               'Assigned_By': (crossRef[3].split(':'))[1]
               }
 #    print goaRec['DB:Reference']
-
-    if len(fields) == 17: # Two extra fields are defined for GAF20FIELDS (GAF 2.0)
+    # Two extra fields are defined for GAF20FIELDS (GAF 2.0):
+    if len(fields) == 17: 
         goaRec['Annotation_Extension'] = ''
         goaRec['Gene_Product_Form_ID'] = ''
     return goaRec
@@ -209,33 +216,50 @@ def appendSprot2goa(fh_sprot, goa_file_name, taxon_id, fh_merged_go):
     goa_dict = {}
     for ingen in iter_handle:
         if ingen['DB_Object_ID'] in goa_dict.keys():
-            goa_dict[ingen['DB_Object_ID']].append([ingen['GO_ID'], ingen['Evidence'], ingen['Aspect']])
+            goa_dict[ingen['DB_Object_ID']].append([ingen['GO_ID'], \
+                            ingen['Evidence'], ingen['Aspect']])
         else:
-            goa_dict[ingen['DB_Object_ID']] = [[ingen['GO_ID'], ingen['Evidence'], ingen['Aspect']]]
+            goa_dict[ingen['DB_Object_ID']] = [[ingen['GO_ID'], \
+                           ingen['Evidence'], ingen['Aspect']]]
 
-# EXTRACTS the NEW GO terms in t2 file that are NOT found in t1 file
+    # EXTRACTS the NEW GO terms in t2 file that are NOT found in t1 file:
     goCount = 0
     for rec in sp.parse(fh_sprot):
-        if taxon_id in rec.taxonomy_id: # SELECTS records that are related to a specific taxon_id such as 559292 for yeast
-            for ac in range(len(rec.accessions)): # Going over each of the entries of the accessions list
-                knownProt = "" # knownProt is an indicator to detect whether the current sprot protein is already in GOA file
+        # SELECTS records that are related to a specific taxon_id
+        # such as 559292 for yeast:
+        if taxon_id in rec.taxonomy_id: 
+            # Going over each of the entries of the accessions list:
+            for ac in range(len(rec.accessions)): 
+                # knownProt is an indicator to detect whether the 
+                # current sprot protein is already in GOA file:
+                knownProt = "" 
                 if rec.accessions[ac] in goa_dict.keys():
+                    # If the current sprot protein is already in the GOA
+                    # file, the sprot protein is assigned to knownProt:
                     knownProt = rec.accessions[ac]
-                        # If the current sprot protein is already in the GOA file, the sprot protein is assigned to knownProt 
                     break
-            for crossRef in rec.cross_references: # Going over the list of GO information
-                if crossRef[0] == 'GO': # consider the cross_reference entries that relate to GO DB
-                    goList = [crossRef[1], (crossRef[3].split(':'))[0], crossRef[2][0]]
-                    # goList is a list of GO ID, Aspect, and Evidence
-                    if (not knownProt) or (knownProt and goList not in goa_dict[knownProt]):# Checking whether a new GO annotaion found
+            # Going over the list of GO information:
+            for crossRef in rec.cross_references: 
+                # Consider the cross_reference entries that relate to GO DB:
+                if crossRef[0] == 'GO':
+                    # goList is a list of GO ID, Aspect, and Evidence:
+                    goList = [crossRef[1], (crossRef[3].split(':'))[0], \
+                              crossRef[2][0]]
+                    # Checking whether a new GO annotaion found:
+                    if (not knownProt) or (knownProt and \
+                        goList not in goa_dict[knownProt]):
                         # A new GO annotation is found in two situations:
                         # 1. if knownProt is empty  (not knownProt) or
-                        # 2. if knownProt is not empty but the GO annotation is not found in the GOA file
-                        goaRec = swissProt2GOA(rec, crossRef, GAFFIELDS) # Convert the sprot record to a GOA record
-                        GOAParser.writerec(goaRec, fh_merged_go, GAFFIELDS) # Write the converted GOA record to the output file
+                        # 2. if knownProt is not empty but the GO annotation
+                        #    is not found in the GOA file
+
+                        # Convert the sprot record to a GOA record:
+                        goaRec = swissProt2GOA(rec, crossRef, GAFFIELDS) 
+                        # Write the converted GOA record to the output file:
+                        GOAParser.writerec(goaRec, fh_merged_go, GAFFIELDS) 
 #                        if goCount in range(1, 20) or goCount in range(6400, 6420):
 #                            print ('goCount: ' + str(goCount) + '\n')
-#                            goaRec = swissProt2GOA(rec, crossRef, GAFFIELDS) # Convert the sprot record to a GOA record
+#                            goaRec = swissProt2GOA(rec, crossRef, GAFFIELDS)
                         goCount += 1
 
 if __name__ == '__main__':
