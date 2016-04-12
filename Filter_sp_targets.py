36894f0dea9c4d1da0bdce420cfe767afe3f4eb1
arkatebi <arkatebi@gmail.com>
2016-04-12 10:41:59 -0500

    Updated filter_species method for creating map file

--- Filter_sp_targets.py
+++ Filter_sp_targets.py
@@ -1,71 +1,131 @@
 #!/usr/bin/env python
 
-import sys
+'''
+    This module has the following two methods:
 
+    species_filter:
+        This method takes four input arguments:
+            (1) a uniprot-swissProt file handle,
+            (2) a taxonomy id,
+            (3) an output file handle for writing target sequences,
+            (4) an output file handle for writing the mapping between
+                target id and protein name, and
+            (5) the set of EXP codes.
+            
+        If the function finds a protein that does NOT have any EXP evidence 
+        code, it writes the protein sequence for that protein to the output 
+        file. It also writes the mapping of target id and protein name to 
+        the map file.
+
+        It returns the following TWO values:
+            Total number of sequences in the sprot file related to the the 
+                taxonomy id
+            Total number of sequences in the sprot file related to the the 
+                taxonomy id whose annotations have EXP evidence
+
+    species_filter_count:
+        This method takes three arguments:
+            (1) a uniprot-swissProt file handle
+            (2) a taxonomy id
+            (3) the set of EXP codes
+
+        It returns TWO values:
+            Total number of sequences in the sprot file related to the the 
+                taxonomy id
+            Total number of sequences in the sprot file related to the the 
+                taxonomy id whose annotations have EXP evidence
+'''
+
+import sys
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio import SwissProt as sp
 
-'''
-     This species_filter method takes four input arguments: (1) a 
-     uniprot-swissProt file handle, (2) a taxonomy id, (3) an output file 
-     handle for writing target sequences, (4) an output file handle for 
-     writing the mapping between target id and protein name, and (5) the 
-     set of EXP codes. If the function finds a protein that does NOT have 
-     any EXP evidence code, it writes the protein sequence for that protein 
-     to the output file. It also writes the mapping of target id and protein 
-     name to the map file.
-'''
-
-def species_filter(fh_sprot, taxon_id, fh_targets, fh_map, EXP_default=set([])):
+def species_filter(fh_sprot, taxon_id, fh_targets, 
+                   fh_map, EXP_default=set([])):
+    # Initializes the target_id:
     target_id = int(taxon_id+"0000001")
     outseq_list = []
+
+    # Counts total number of sequences 
+    # in the sprot file related to the the taxonomy id taxon_id:
     seqCount = 0
-    seqCount_exp = 0 
+
+    # Counts total number of sequences in the sprot file related 
+    # to the the taxonomy id taxon_id whose annotations have EXP 
+    # evidence:
+    seqCount_no_exp = 0
+
     for rec in sp.parse(fh_sprot):
-        if taxon_id in rec.taxonomy_id: # Selects records that are related to a specific taxon_id such as 559292 for yeast
-            exp_code = 0 
+        # Selects records that are related to a specific
+        # taxonomy id taxon_id:
+        if taxon_id in rec.taxonomy_id:
+            exp_code = False 
             seqCount += 1
-            for crossRef in rec.cross_references: # Going over the list of GO information
-                if crossRef[0] == 'GO': # consider the cross_reference entries that relate to GO DB
-                    goList = [crossRef[1], (crossRef[3].split(':'))[0], crossRef[2][0]]
+            # Going over the list of GO information:
+            for crossRef in rec.cross_references: 
+                # Consider the cross_reference entries 
+                # that relate to GO DB:
+                if crossRef[0] == 'GO':
+                    goList = [crossRef[1], 
+                             (crossRef[3].split(':'))[0], 
+                             crossRef[2][0]]
                     if (crossRef[3].split(':'))[0] in EXP_default:
-                        exp_code = 1
+                        exp_code = True
                         break
-            if not exp_code: # if the protein does not have any experimental validation, then write out the sequence
+            # If the protein has no EXP evidence,
+            # write the sequence to the output file:
+            if not exp_code:
                 outseq = SeqRecord(Seq(rec.sequence),
                        id="T"+str(target_id),
                        description = "%s" %
                        (rec.entry_name))
                 outseq_list = [outseq]
-                SeqIO.write(outseq_list,fh_targets, "fasta") 
-                                      # write out the sequence
-                mapStr = "T"+str(target_id) + '\t' + str(rec.entry_name) + '\n'
+                # Write out the sequence:
+                SeqIO.write(outseq_list,fh_targets, "fasta")
+                mapStr = "T" + str(target_id) + '\t' + \
+                               str(rec.entry_name) + '\n'
+                # Write out the mapping (target id -> protein name):
                 fh_map.write("%s" % mapStr)
-                            # write out the mapping (target id -> protein name)
                 target_id += 1
-                seqCount_exp += 1
-    return (seqCount, seqCount_exp)
+                seqCount_no_exp += 1
+#    return (seqCount, seqCount_no_exp)
+    return seqCount_no_exp
 
 def species_filter_count(fh_sprot, taxon_id, EXP_default=set([])):
+    # The seqCopunt variable counts total number of sequences
+    # in the sprot file related to the the taxonomy id taxon_id:
     seqCount = 0
-    seqCount_exp = 0 
+
+    # The seqCount_exp variable counts total number of sequences in 
+    # the sprot file related to the the taxonomy id taxon_id whose 
+    # annotations have EXP evidence:
+    seqCount_no_exp = 0
+
     for rec in sp.parse(fh_sprot):
-        if taxon_id in rec.taxonomy_id: # SELECTS records that are related to a specific taxon_id such as 559292 for yeast
-            exp_code = 0 
+        # SELECT records that are related to a specific
+        # taxon_id such as 559292 for yeast:
+        if taxon_id in rec.taxonomy_id:
+            exp_code = False
             seqCount += 1
-            for crossRef in rec.cross_references: # Going over the list of GO information
-                if crossRef[0] == 'GO': # consider the cross_reference entries that relate to GO DB
-                    goList = [crossRef[1], (crossRef[3].split(':'))[0], crossRef[2][0]]
+            # Go over the list of GO information:
+            for crossRef in rec.cross_references:
+                # Consider the cross_reference entries that 
+                # relate to GO DB:
+                if crossRef[0] == 'GO':
+                    goList = [crossRef[1],
+                              (crossRef[3].split(':'))[0], 
+                              crossRef[2][0]]
                     if (crossRef[3].split(':'))[0] in EXP_default:
-                        exp_code = 1
+                        exp_code = True
                         break
-            if not exp_code: # if the protein does not have any experimental validation, increase seqCount_exp 
-                seqCount_exp += 1
-    return (seqCount, seqCount_exp)
+            # If the protein has an no EXP evidence,
+            # increase seqCount_no_exp:
+            if not exp_code:
+                seqCount_no_exp += 1
+    return (seqCount, seqCount_no_exp)
 
 if __name__ == '__main__':
-    print 'This program does not run independently'
-    sys.exit(1)
-     
+    print(__doc__)
+    sys.exit(0)
